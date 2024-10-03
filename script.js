99245437304a65dcd40d36154d909c76d0d2e1a1
Terezie Mazáčová <terezkamazacova@post.cz>
2024-10-03 13:32:18 +0200

    scss struktura celeho slideru v sekci Our Testimonials

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 document.addEventListener( 'DOMContentLoaded', function() {
     var splide = new Splide( '.splide',{
         perPage: 3,
-        rewind : true,
+        gap: '20px',
+        arrows: true,
+        type: 'loop',
     } );
     splide.mount();
 });
