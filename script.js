a706aab6d0626424c8fd354fc31d0dc3dfac6a45
Terezie Mazáčová <terezkamazacova@post.cz>
2024-10-03 11:59:20 +0200

    zalozena stranka pro js, slider, pouzita zakladni struktura splide.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+    var splide = new Splide( '.splide',{
+        perPage: 3,
+        rewind : true,
+    } );
+    splide.mount();
+});
