627321588e83ec403f3c9e79f5ebb345e15beac4
Terezie Mazáčová <terezkamazacova@post.cz>
2024-10-05 13:51:46 +0200

    cela sekce our pricing (html, scss, js)

--- script.js
+++ script.js
@@ -7,3 +7,26 @@ document.addEventListener( 'DOMContentLoaded', function() {
     } );
     splide.mount();
 });
+
+
+// == SECTION OUR PRICING ==
+const jsTabs = document.querySelectorAll('.js-tabs')
+
+jsTabs.forEach((jsTab) =>{
+    const tabs = jsTab.querySelectorAll("[data-tab-target]");
+    const tabContents = jsTab.querySelectorAll("[data-tab-content]");
+
+    tabs.forEach((tab) => {
+        tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.tabTarget);
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove("active");
+        });
+        tabs.forEach((tab) => {
+            tab.classList.remove("active");
+        });
+        tab.classList.add("active");
+        target.classList.add("active");
+        });
+    });
+})
