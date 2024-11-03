56cc7db6c2833d9cefdc34db1aa593b1720607c0
Terezie Mazáčová <terezkamazacova@post.cz>
2024-11-03 22:53:47 +0100

    Breakpointy pro Slider na hlavni strance

--- script.js
+++ script.js
@@ -18,6 +18,25 @@ document.addEventListener( 'DOMContentLoaded', function() {
         gap: '20px',
         arrows: true,
         type: 'loop',
+        //height: '400px',
+        breakpoints: {
+            1200: {
+                perPage: 3,
+                gap: '15px',
+                //height: '550px'
+            },
+            992: {
+                perPage: 2,
+                gap: '10px',
+                //height: '450px'
+                
+            },
+            768: {
+                perPage: 1,
+                gap: '5px',
+                //height: '450px'
+            }
+        }
     } );
     splide.mount();
 });
